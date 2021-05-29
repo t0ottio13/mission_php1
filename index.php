fa58703d4bb0ffcd2455b1c8bb55d5b1cf49343d
t0ottio13 <adsoption104@gmail.com>
2021-05-29 22:14:10 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Document</title>
+</head>
+<body>
+    <h1>New User</h1>
+    <form action="./todo_get_confirm.php" method="POST">
+        <fieldset>
+            <legend>ユーザー登録画面</legend>
+            <div>
+            User Name: <input type="name" name="user_name">
+            </div>
+            <div>
+            email: <input type="email" name="email">
+            </div>
+            <div>
+            <button>submit</button>
+            </div>
+        </fieldset>
+    </form>
+</body>
+</html>
